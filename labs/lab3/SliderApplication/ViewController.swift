d0cbf059a26f3e5655981003ebdd16733cb0343e
varunknarayanswamy <32069048+varunknarayanswamy@users.noreply.github.com>
2019-09-23 14:28:24 -0600

    adding in color functionality

--- labs/lab3/SliderApplication/ViewController.swift
+++ labs/lab3/SliderApplication/ViewController.swift
@@ -14,8 +14,11 @@ class ViewController: UIViewController {
     @IBOutlet weak var CapsOutlet: UILabel!
     @IBOutlet weak var FamFriendImage: UIImageView!
     @IBOutlet weak var FFLabel: UILabel!
+    @IBOutlet var TheView: UIView!
     @IBOutlet weak var capsSwitch: UISwitch!
+    @IBOutlet weak var ColorSegment: UISegmentedControl!
     @IBOutlet weak var imageControl: UISegmentedControl!
+    @IBOutlet weak var SlidinIntoDMS: UISlider!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -35,6 +38,31 @@ class ViewController: UIViewController {
         {
             CapsOutlet.text = CapsOutlet.text?.lowercased()
             FFLabel.text = FFLabel.text?.lowercased()
+        }
+    }
+    @IBAction func ColorFunction(_ sender: UISegmentedControl) {
+        if (sender.selectedSegmentIndex == 1)
+        {
+            FFLabel.textColor = UIColor.blue
+            sender.tintColor = UIColor.blue
+            imageControl.tintColor = UIColor.blue
+            SizeOutlet.textColor = UIColor.blue
+            CapsOutlet.textColor = UIColor.blue
+            SlidinIntoDMS.tintColor = UIColor.blue
+            TheView.backgroundColor = UIColor.white
+            capsSwitch.onTintColor = UIColor.blue
+        }
+        else
+        {
+            FFLabel.textColor = UIColor.red
+            sender.tintColor = UIColor.red
+            imageControl.tintColor = UIColor.red
+            SizeOutlet.textColor = UIColor.red
+            CapsOutlet.textColor = UIColor.red
+            SlidinIntoDMS.tintColor = UIColor.red
+            TheView.backgroundColor = UIColor.black
+            capsSwitch.onTintColor = UIColor.red
+            
         }
     }
     @IBAction func selectFF(_ sender: UISegmentedControl) {
