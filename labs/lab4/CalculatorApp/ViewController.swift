fa3aa1d1894636e06f39585b90c4b4b0d24346f1
varunknarayanswamy <32069048+varunknarayanswamy@users.noreply.github.com>
2019-10-02 17:47:58 -0600

    adding in lab4, a lab where we explore textfields, steppers, delegates and protocols, and for me, scrollviews

---
+++ labs/lab4/CalculatorApp/ViewController.swift
@@ -0,0 +1,89 @@
+//
+//  ViewController.swift
+//  CalculatorApp
+//
+//  Created by Varun Narayanswamy on 9/26/19.
+//  Copyright © 2019 Varun Narayanswamy LPC. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController, UITextFieldDelegate {
+    @IBOutlet weak var TotalHoursOfWork: UITextField!
+    @IBOutlet weak var NumberOfDaysGiven: UITextField!
+    @IBOutlet weak var stepperPerson: UIStepper!
+    @IBOutlet weak var PeopleNum: UILabel!
+    
+    @IBOutlet weak var WorkPerPerson: UILabel!
+    @IBOutlet weak var WorkPerDay: UILabel!
+    var people = 1
+    let TextFieldDelegate = UITextFieldDelegate.self
+    override func viewDidLoad() {
+        TotalHoursOfWork.delegate = self
+        NumberOfDaysGiven.delegate = self
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+    @IBAction func ChangeNumPeople(_ sender: UIStepper) {
+        people = Int(sender.value)
+        if (people>1)
+        {
+            PeopleNum.text = "\(people) People"
+            TextfieldCheck()
+        }
+        else if (people==1)
+        {
+            PeopleNum.text = "\(people) Person"
+            TextfieldCheck()
+        }
+        else
+        {
+            
+            let Alert = UIAlertController(title: "Too Few People", message: "At least 1 person better do work", preferredStyle: UIAlertController.Style.alert)
+            let cancel  = UIAlertAction(title: "cancel", style: UIAlertAction.Style.cancel, handler: nil)
+            Alert.addAction(cancel)
+            let okay  = UIAlertAction(title: "OK", style: UIAlertAction.Style.default, handler: {action in
+                self.stepperPerson.value = 1
+                self.TextfieldCheck()
+            })
+            Alert.addAction(okay)
+            present(Alert, animated: true, completion: nil)
+        }
+    }
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        textField.resignFirstResponder()
+        TextfieldCheck()
+        return true
+    }
+    
+    func textFieldDidEndEditing(_ textField: UITextField) {
+        textField.resignFirstResponder()
+        TextfieldCheck()
+    }
+    
+    func TextfieldCheck() -> Bool {
+        if (NumberOfDaysGiven.text?.isEmpty == false && TotalHoursOfWork.text?.isEmpty == false)
+        {
+            let totalTimeGiven = Float(NumberOfDaysGiven.text!)
+            let TotalHours = Float(TotalHoursOfWork.text!)
+            let totalHoursPerPerson = TotalHours!/Float(people)
+            let hoursPerDay = totalHoursPerPerson/totalTimeGiven!
+            WorkPerPerson.text = String(totalHoursPerPerson)
+            WorkPerDay.text = String(hoursPerDay)
+            return true
+        }
+        else
+        {
+            return false
+        }
+    }
+}
+
+extension NSLayoutConstraint {
+    
+    override open var description: String {
+        let id = identifier ?? ""
+        return "id: \(id), constant: \(constant)" //you may print whatever you want here
+    }
+}
+
---
+++ labs/lab4/CalculatorAppTests/CalculatorAppTests.swift
@@ -0,0 +1,34 @@
+//
+//  CalculatorAppTests.swift
+//  CalculatorAppTests
+//
+//  Created by Varun Narayanswamy on 9/26/19.
+//  Copyright © 2019 Varun Narayanswamy LPC. All rights reserved.
+//
+
+import XCTest
+@testable import CalculatorApp
+
+class CalculatorAppTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
